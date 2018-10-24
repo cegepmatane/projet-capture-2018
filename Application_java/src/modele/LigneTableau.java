37fe2e720babaa97e7efefe69d6f09b7e070cb16
Florent F <florent.fli@gmail.com>
2018-10-24 14:54:39 -0400

    vue tableau avec enregistrement

--- Application_java/src/modele/LigneTableau.java
+++ Application_java/src/modele/LigneTableau.java
@@ -1,4 +1,39 @@
 package modele;
 
 public class LigneTableau {
+    double moyene;
+    double maximum;
+    double minimum;
+
+    public LigneTableau(){}
+
+    public LigneTableau(double moyene, double maximum, double minimum) {
+        this.moyene = moyene;
+        this.maximum = maximum;
+        this.minimum = minimum;
+    }
+
+    public double getMoyene() {
+        return moyene;
+    }
+
+    public void setMoyene(double moyene) {
+        this.moyene = moyene;
+    }
+
+    public double getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(double maximum) {
+        this.maximum = maximum;
+    }
+
+    public double getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(double minimum) {
+        this.minimum = minimum;
+    }
 }
--- Application_java/src/vue/VueHumidite.java
+++ Application_java/src/vue/VueHumidite.java
@@ -8,18 +8,22 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import modele.LigneTableau;
 
 public class VueHumidite extends Scene {
     Pane panneau;
     Controleur controleur;
     GridPane grille;
 
-    TableView<String> tableau;
+    TableView<LigneTableau> tableau;
 
     public VueHumidite() {
         super(new Pane(), 1060, 650);
+
+        super.getStylesheets().add("style/style.css");
 
         this.panneau = (Pane) this.getRoot();
 
@@ -33,25 +37,29 @@ public class VueHumidite extends Scene {
         tableau.setEditable(false);
 
         TableColumn coloneMoyenne = new TableColumn("Moyenne");
+        coloneMoyenne.setCellValueFactory(new PropertyValueFactory<>("moyene"));
         coloneMoyenne.setResizable(false);
         coloneMoyenne.setMinWidth(320);
 
         TableColumn coloneMin = new TableColumn("Minimum");
+        coloneMin.setCellValueFactory(new PropertyValueFactory<>("minimum"));
         coloneMin.setResizable(false);
         coloneMin.setMinWidth(320);
 
         TableColumn coloneMax = new TableColumn("Maximum");
+        coloneMax.setCellValueFactory(new PropertyValueFactory<>("maximum"));
         coloneMax.setResizable(false);
         coloneMax.setMinWidth(320);
 
         tableau.setMinWidth(960);
-
         tableau.getColumns().addAll(coloneMoyenne, coloneMin, coloneMax);
 
+        LigneTableau ligne = new LigneTableau(34, 55.4, 1.2);
+        tableau.getItems().add(ligne);
 
-        grille.add(new Label("Vue Humiditée :"), 0,0);
+        grille.add(new Label("Vue Humiditée :"), 0, 0);
         grille.setVgap(50);
-        grille.add(tableau,0,1);
+        grille.add(tableau, 0, 1);
 
         panneau.getChildren().add(grille);
     }
