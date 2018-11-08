c14298048cac102092ae65e28386c87cecd10394
Florent F <florent.fli@gmail.com>
2018-11-08 11:09:46 -0500

    affichage selon echantillonnage

--- Application_java/src/controleur/Controleur.java
+++ Application_java/src/controleur/Controleur.java
@@ -1,13 +1,17 @@
 package controleur;
 
+import accesseur.HumiditeDAO;
 import vue.NavigateurDesVues;
 import vue.VueHumidite;
 
 import java.util.List;
 
+import static accesseur.Connection.URL_BASE;
+
 public class Controleur {
     private NavigateurDesVues navigateurDesVues;
     private VueHumidite vueHumidite;
+    private HumiditeDAO humiditeDAO = new HumiditeDAO();
 
     private Controleur() {}
 
@@ -24,5 +28,21 @@ public class Controleur {
         this.navigateurDesVues = navigateurDesVues;
 
         this.vueHumidite = this.navigateurDesVues.getVueHumidite();
+
+        this.vueHumidite.afficher(humiditeDAO.listerToutesHumidite());
+    }
+
+    public void notifierActualiser() {
+        String url = URL_BASE;
+
+        String echantillonnage = navigateurDesVues.getVueHumidite().getBoiteChoix().getValue();
+        String dateDebut = navigateurDesVues.getVueHumidite().getDateChoixDebut().getValue().toString();
+        String dateFin = navigateurDesVues.getVueHumidite().getDateChoixFin().getValue().toString();
+
+        url += "/"+echantillonnage;
+
+        System.out.println(url);
+
+        navigateurDesVues.getVueHumidite().afficher(humiditeDAO.listerHumiditeSelonURL(url));
     }
 }
--- Application_java/src/vue/VueHumidite.java
+++ Application_java/src/vue/VueHumidite.java
@@ -4,13 +4,16 @@ import accesseur.HumiditeDAO;
 import controleur.Controleur;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import modele.Humidites;
 import modele.LigneTableau;
 
 import java.time.LocalDate;
@@ -25,39 +28,51 @@ public class VueHumidite extends Scene {
     ChoiceBox<String> boiteChoix;
     TableView<LigneTableau> tableau;
 
-    DatePicker dateChoixDebut = new DatePicker();
-    DatePicker dateChoixFin = new DatePicker();
+    DatePicker dateChoixDebut;
+    DatePicker dateChoixFin;
+
+    Button actionActualiser;
 
     HumiditeDAO humiditeDAO = new HumiditeDAO();
 
     public VueHumidite() {
         super(new Pane(), 1060, 650);
-
         super.getStylesheets().add("style/style.css");
 
         this.panneau = (Pane) this.getRoot();
-
         info = new VBox();
         general = new VBox();
         grille = new GridPane();
         controleur = Controleur.getInstance();
+    }
+
+    public void afficher(Humidites listeHumidite) {
+        this.grille.getChildren().clear();
+        this.info.getChildren().clear();
+        this.general.getChildren().clear();
+        this.panneau.getChildren().clear();
 
         boiteChoix = new ChoiceBox<String>();
-        boiteChoix.getItems().add("Seconde");
-        boiteChoix.getItems().add("Minute");
-        boiteChoix.getItems().add("Heure");
-        boiteChoix.getItems().add("Jours");
-        boiteChoix.getItems().add("Semaine");
-        boiteChoix.getItems().add("Mois");
+        boiteChoix.getItems().add("seconde");
+        boiteChoix.getItems().add("minute");
+        boiteChoix.getItems().add("heure");
+        boiteChoix.getItems().add("jour");
+        boiteChoix.getItems().add("semaine");
+        boiteChoix.getItems().add("mois");
         boiteChoix.getSelectionModel().selectFirst();
 
+        dateChoixDebut = new DatePicker();
+        dateChoixFin = new DatePicker();
         dateChoixDebut.setValue(LocalDate.now());
         dateChoixFin.setValue(LocalDate.now().plusDays(7));
 
-
-        //TODO LISTENER CHOICE BOX
-
-
+        actionActualiser = new Button("Actualiser");
+        actionActualiser.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                controleur.notifierActualiser();
+            }
+        });
         this.tableau = new TableView();
 
         tableau.setEditable(false);
@@ -89,27 +104,49 @@ public class VueHumidite extends Scene {
 
         tableau.setMinWidth(960);
         tableau.setId("tableau-humidite");
-        tableau.getColumns().addAll(coloneDate,coloneMoyenne, coloneMin, coloneMax,coloneNombre);
+        tableau.getColumns().addAll(coloneDate, coloneMoyenne, coloneMin, coloneMax, coloneNombre);
 
-        for (LigneTableau ligne: humiditeDAO.listerToutesHumidite().recupererLignesPourTableau()) {
+        for (LigneTableau ligne : listeHumidite.recupererLignesPourTableau()) {
             tableau.getItems().add(ligne);
         }
 
         //grille.setVgap(50);
         general.setPadding(new Insets(50));
         grille.setVgap(21);
-        grille.add(new Label("Vue Humiditée :"),0,0);
-        grille.add(new Label("Echantillonage :"),0,1);
-        grille.add(boiteChoix,1,1);
-        grille.add(new Label("Date de début :"),0,2);
-        grille.add(dateChoixDebut,1,2);
-        grille.add(new Label("Date de fin :"),0,3);
-        grille.add(dateChoixFin,1,3);
+        grille.add(new Label("Vue Humiditée :"), 0, 0);
+        grille.add(new Label("Echantillonage :"), 0, 1);
+        grille.add(boiteChoix, 1, 1);
+        grille.add(new Label("Date de début :"), 0, 2);
+        grille.add(dateChoixDebut, 1, 2);
+        grille.add(new Label("Date de fin :"), 0, 3);
+        grille.add(dateChoixFin, 1, 3);
+        grille.add(actionActualiser, 1, 4);
         info.getChildren().add(tableau);
 
         general.getChildren().add(grille);
         general.getChildren().add(tableau);
-
         panneau.getChildren().add(general);
     }
+
+    public ChoiceBox<String> getBoiteChoix() {
+        return boiteChoix;
+    }
+
+    public TableView<LigneTableau> getTableau() {
+        return tableau;
+    }
+
+    public DatePicker getDateChoixDebut() {
+        return dateChoixDebut;
+    }
+
+    public DatePicker getDateChoixFin() {
+        return dateChoixFin;
+    }
+
+    public Button getActionActualiser() {
+        return actionActualiser;
+    }
+
+
 }
